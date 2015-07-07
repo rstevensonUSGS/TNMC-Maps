6a5431359385ad44637b16f8deb12190c9073bce
Ricardo-C-Oliveira <oliveira.ricardo@hotmail.com>
2015-07-07 09:52:47 -0600

    add the all fetures id to the correct function

--- uneditedpoints/mainMap/script.js
+++ uneditedpoints/mainMap/script.js
@@ -99,7 +99,7 @@ $('#statelayers li').click(function() {
     //add a header with the state name
     $("#selectedState").html("<p>Unedited Points in " + stateName + "");
     //remove dissabled class from filters
-    $('.filter').removeClass("disabled");
+    $('.filter, #all, #reset').removeClass("disabled");
     geoJson = L.geoJson(data, {
       pointToLayer: function(feature, latlng) {
         var popupContent = '<a href="http://navigator.er.usgs.gov/edit?node=' + feature.properties.OSM_ID + '" target="_blank">Edit this point</a>';
